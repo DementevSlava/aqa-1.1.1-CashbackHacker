7cc952cd3e9a25713295150f18105e375bdd97e7
Dementev Slava <kursant344@gmail.com>
2020-05-29 12:21:48 +0300

    add JunitJupiter

--- src/test/java/ru/netology/service/CashbackHackServiceApiJunitJJupiterTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceApiJunitJJupiterTest.java
@@ -1,10 +1,11 @@
 package ru.netology.service;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
-public class CashbackHackServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceApiJunitJJupiterTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
 
     @Test
